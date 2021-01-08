4059811e410830f1e10919b3245f6b9da38c8269
Monish Prabu Chandran <monish.chandran@salesforce.com>
2021-01-08 14:19:19 -0800

    hack

--- modules/slackvance.js
+++ modules/slackvance.js
@@ -12,39 +12,21 @@ exports.handle = (req, res) => {
     }
 
     let slackUserId = req.body.user_id,
-        oauthObj = auth.getOAuthObject(slackUserId),
-        q = "SELECT Id, Name, Phone, MobilePhone, Email FROM Contact WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
-
-	options = {
-		method: 'POST',
-		model : "{\"approvalId\":\"a061h000002pIlTAAU\"}",
-		saver : "SBAA.ApprovalRestApiProvider.Approve"
-	};
+        oauthObj = auth.getOAuthObject(slackUserId);
+	let options = {
+			method: 'POST',
+			model : "{\"approvalId\":\"a061h000002pIlTAAU\"}",
+			saver : "SBAA.ApprovalRestApiProvider.Approve"
+		};
 	console.log(req.body);
-    force.query(oauthObj, q)
-        .then(data => {
-            let contacts = JSON.parse(data).records;
-            if (contacts && contacts.length>0) {
-                let attachments = [];
-                contacts.forEach(function(contact) {
-                    let fields = [];
-                    fields.push({title: "Name", value: contact.Name, short:true});
-                    fields.push({title: "Phone", value: contact.Phone, short:true});
-                    fields.push({title: "Mobile", value: contact.MobilePhone, short:true});
-                    fields.push({title: "Email", value: contact.Email, short:true});
-                    fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + contact.Id, short:false});
-                    attachments.push({color: "#A094ED", fields: fields});
-                });
-                res.json({text: "Contacts matching '" + req.body.text + "':", attachments: attachments});
-            } else {
-                res.send("No records");
-            }
-        })
-        .catch(error => {
-            if (error.code == 401) {
-                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
-            } else {
-                res.send("An error as occurred");
-            }
-        });
+	res.send(req.body);
+	// force.apexrest(oauthObj, '/sbaa/ServiceRouter', options).then(data => {
+	// 	res.send(data);
+	// }).catch(error => {
+	// 	if (error.code == 401) {
+	// 		res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
+	// 	} else {
+	// 		res.send(data);
+	// 	}
+	// });
 };
