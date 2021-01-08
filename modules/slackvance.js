da41fd43de03e7d14f95aa838406ceff48b7e501
Monish Prabu Chandran <monish.chandran@salesforce.com>
2021-01-08 15:49:58 -0800

    hack

--- modules/slackvance.js
+++ modules/slackvance.js
@@ -6,39 +6,38 @@ let auth = require("./slack-salesforce-auth"),
 
 exports.handle = (req, res) => {
 	try {
-	console.log("got..", req.body);
-	console.log("got payload..", req.body.payload);
-	console.log("got payload parse..", JSON.parse(req.body.payload));
-	console.log("token...:", req.body.token);
+		let payload = JSON.parse(req.body.payload);
+	
+	console.log("token...:", payload.token);
 	console.log("CONTACT_TOKEN...:", CONTACT_TOKEN);
 
-	if (req.body.token != CONTACT_TOKEN) {
+	if (payload.token != CONTACT_TOKEN) {
 		console.log("invalid token..")
         res.send("Invalid token");
         return;
     }
 
-     let slackUserId = req.body.user_id,
+     let slackUserId = payload.user.id,
          oauthObj = auth.getOAuthObject(slackUserId);
 		// let optionstmp = {
 		// 		method: 'POST',
 		// 		model : "{\"approvalId\":\"a061h000002pIlTAAU\"}",
 		// 		saver : "SBAA.ApprovalRestApiProvider.Approve"
 		// 	};
-		// console.log('slackvance broker got request');
+		console.log('slackvance broker got request');
 		// console.log(req.body);
-		// let modelValue = req.body.actions[0].value;
-		// console.log(modelValue);
-		// let saverValue = '';
-		// if (req.body.actions[0].text.text == 'Approve') {
-		// 	saverValue = 'SBAA.ApprovalRestApiProvider.Approve'
-		// } else {
-		// 	saverValue = 'SBAA.ApprovalRestApiProvider.Reject'
-		// }
-		// let options = {
-		// 	model : modelValue,
-		// 	saver : saverValue
-		// }
+		let modelValue = payload.actions[0].value;
+		console.log(modelValue);
+		let saverValue = '';
+		if (payload.actions[0].text.text == 'Approve') {
+			saverValue = 'SBAA.ApprovalRestApiProvider.Approve'
+		} else {
+			saverValue = 'SBAA.ApprovalRestApiProvider.Reject'
+		}
+		let options = {
+			model : modelValue,
+			saver : saverValue
+		}
 	// console.log(req.body);
 	// console.log('Got approved');
 	res.send("end");
