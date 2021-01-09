38721e8e932894122f02b0a21ac4cd04c7f19a0f
Monish Prabu Chandran <monish.chandran@salesforce.com>
2021-01-08 19:23:54 -0800

    hack

---
+++ slackvance2.js
@@ -0,0 +1,56 @@
+"use strict";
+
+let jsforce = require('jsforce'),
+	request = require('request')
+
+exports.handle = (req, res) => {
+
+		let payload = JSON.parse(req.body.payload);
+		
+		let status = '';
+		let modelValue = {approvalId: payload.actions[0].value};
+		let saverValue = '';
+		if (payload.actions[0].text.text == 'Approve') {
+			saverValue = 'SBAA.ApprovalRestApiProvider.Approve'
+			status = 'approved'
+		} else {
+			saverValue = 'SBAA.ApprovalRestApiProvider.Reject'
+			status = 'rejected'
+		}
+		let options = {
+			"method" : "POST",
+			"body" : {
+				"model" : modelValue,
+				"saver" : saverValue
+			}
+		}
+		console.log('options: ', options);
+		let conn = new jsforce.Connection();
+		conn.login('test-kdqmffi3ivs8@example.com', 'test1234', function(err, res) {
+		if (err) { return console.error(err); }
+			// body payload structure is depending to the Apex REST method interface.
+			conn.apex.post("sbaa/ServiceRouter", options.body, function(err, res) {
+			if (err) { return console.error(err); }
+			else {
+				request.post({
+					uri: payload.response_url,
+					body: {
+						replace_original: false,
+						text: "OK!"
+					},
+					json: true
+				}, function (err, res, body) {
+					//handle callback  
+					if (err) {
+						console.error(err)
+						return
+					  }
+					  console.log(`statusCode: ${res.statusCode}`)
+					  console.log(body)          
+				});
+			}
+				console.log("response: ", res);
+				// the response object structure depends on the definition of apex class
+			});
+		});
+};
